6b468f56c24c5d6633fa83b5cd3f603cc4d23908
Gusti Kanjeng Raden Mas Unan Nurwenda <111562928+KanjengRadenMasUnan@users.noreply.github.com>
2026-07-22 08:36:04 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,4 +4,6 @@
 const isProduction = window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1'
 
 // Ganti URL Render di bawah ini dengan URL backend Render Anda yang sebenarnya
-export const API_BASE_URL = 'http://127.0.0.1:8000/api'
+export const API_BASE_URL = isProduction
+  ? 'https://kkm61backend.onrender.com/api'
+  : `http://${window.location.hostname}:8000/api`
